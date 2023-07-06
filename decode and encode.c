7994ad4584e5d79f9a6b200100e052e85058616a
Sonia <soniaputhran57@gmail.com>
2023-07-06 23:26:26 +0530

    first commit

---
+++ decode and encode.c
@@ -0,0 +1,70 @@
+
+#include <iostream>
+#include<string.h>
+
+
+using namespace std;
+
+void encode(string str,int n)
+{
+    int dup;
+    string str1;
+    string k;
+    cout<<"The encoded message is ";
+    for(int i=0;i<str.length();i++)
+    {
+        int l;
+        dup=int(str[i]);
+        l=dup+n;
+        k=char(l);
+        cout<<k;
+        
+    }
+    
+}
+
+void decode(string str,int n)
+{
+    int dup;
+    string str1;
+    string k;
+    cout<<"The decoded message is ";
+    for(int i=0;i<str.length();i++)
+    {
+        dup=int(str[i]);
+        int l=dup-n;
+        if(l<97)
+        {
+            l=97-l;
+            l=123-l;
+        }
+        k=char(l);
+        cout<<k;
+        
+    }
+}
+
+int main()
+{
+    string s,str;
+    int n;
+    cout<<"Type encode to encrypt and decode to decrypt: ";
+    cin>>s;
+    cout<<"Type the message: ";
+    cin>>str;
+    cout<<"Enter the number of shifts: ";
+    cin>>n;
+    if(s=="encode")
+    {
+        encode(str,n);
+    }
+    else if(s=="decode")
+    {
+        decode(str,n);
+    }
+    else
+    {
+        cout<<"Invalid input!!!\n";
+    }
+    return 0;
+}
