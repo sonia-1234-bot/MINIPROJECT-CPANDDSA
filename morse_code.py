243e703412f30de07d2d46cce7c231ba61f1caf6
Sonia <soniaputhran57@gmail.com>
2023-11-15 12:48:57 +0530

    initial commit

---
+++ morse_code.py
@@ -0,0 +1,31 @@
+morse_code={"A":".-","B":"-...","C":"-.-.","D":"-..","E":".","F":"..-.","G":"--.","H":"....","I":"..","J":".---","K":"-.-","L":".-..","M":"--","N":"-.","O":"---","P":".--.","Q":"--.-","R":".-.","S":"...","T":"-","U":"..-","V":"...-","W":".--","X":"-..-","Y":"-.--","Z":"--.."}
+
+def find_key(val):
+    for key,value in morse_code.items():
+        if value==val:
+            return key
+
+while True:
+    choice=int(input('''Type 1 to convet Text to Morse Code
+Type 2 to convert Morse Code to Text
+Type 3 to exit 
+'''))
+
+    if choice==1:
+        text1=input("Enter the text you want to convert: ")
+        text=text1.upper()
+        print(f"{text1} in Morse code is: ",end=" ")
+        for i in text:
+            print(morse_code.get(i),end=" ")
+        print()    
+    elif choice==2:
+        morse=input("Enter the morse code with space for letter: ")
+        print(f"{morse} in letter is: ",end=" ")
+        m=morse.split(' ')    
+        for i in m:
+            print(find_key(i),end="")
+        print()      
+    elif choice==3:
+        exit()
+    else: 
+        print("Invalid choice!!!")            
