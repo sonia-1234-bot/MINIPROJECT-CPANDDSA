0723880abf1c88d784df34e15842108acb159169
Sonia <soniaputhran57@gmail.com>
2023-11-15 20:18:07 +0530

    final commit

--- morse_code.py
+++ morse_code.py
@@ -1,3 +1,23 @@
+import pygame
+import time
+
+pygame.init()
+
+pygame.mixer.init()
+def code_sound(dot_dash):
+    for j in dot_dash:
+        if j==".":
+            print(".",end="")
+            sound=pygame.mixer.Sound("ping-82822.mp3")
+            sound.play()
+            time.sleep(1)
+        elif j=="-":
+            print("-",end="")
+            sound1=pygame.mixer.Sound("beep-1-sec-6162.mp3")
+            sound1.play()  
+            time.sleep(1)  
+    print(end=" ")    
+
 morse_code={"A":".-","B":"-...","C":"-.-.","D":"-..","E":".","F":"..-.","G":"--.","H":"....","I":"..","J":".---","K":"-.-","L":".-..","M":"--","N":"-.","O":"---","P":".--.","Q":"--.-","R":".-.","S":"...","T":"-","U":"..-","V":"...-","W":".--","X":"-..-","Y":"-.--","Z":"--.."}
 
 def find_key(val):
@@ -15,8 +35,9 @@ Type 3 to exit
         text1=input("Enter the text you want to convert: ")
         text=text1.upper()
         print(f"{text1} in Morse code is: ",end=" ")
-        for i in text:
-            print(morse_code.get(i),end=" ")
+        for k in text:
+            #print(morse_code.get(i),end=" ")
+            code_sound(morse_code.get(k))
         print()    
     elif choice==2:
         morse=input("Enter the morse code with space for letter: ")
